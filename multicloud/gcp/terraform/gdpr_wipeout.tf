e84a61fa0724bcf18a5c5faf024da656c4d90edd
przemeksroka <przemeksroka@google.com>
2026-03-04 02:56:35 +0100

    feat: Add Scheduler trigger for gdpr-wipeout job every hour

--- multicloud/gcp/terraform/gdpr_wipeout.tf
+++ multicloud/gcp/terraform/gdpr_wipeout.tf
@@ -5,7 +5,8 @@ resource "google_project_service" "gdpr_wipeout_apis" {
   for_each = toset([
     "run.googleapis.com",
     "artifactregistry.googleapis.com",
-    "compute.googleapis.com"
+    "compute.googleapis.com",
+    "cloudscheduler.googleapis.com"
   ])
   project            = var.project_id
   service            = each.key
@@ -62,5 +63,48 @@ resource "google_cloud_run_v2_job" "gdpr_wipeout_job" {
   depends_on = [
     google_project_service.gdpr_wipeout_apis,
     google_artifact_registry_repository.crm_jobs_repo
+  ]
+}
+
+# Service account for Cloud Scheduler to invoke the Cloud Run Job
+resource "google_service_account" "gdpr_wipeout_invoker" {
+  account_id   = "gdpr-wipeout-invoker"
+  display_name = "Cloud Scheduler SA for GDPR Wipeout"
+  project      = var.project_id
+}
+
+# Grant the service account permissions to invoke the job
+resource "google_cloud_run_v2_job_iam_member" "gdpr_wipeout_invoker_binding" {
+  project  = google_cloud_run_v2_job.gdpr_wipeout_job.project
+  location = google_cloud_run_v2_job.gdpr_wipeout_job.location
+  name     = google_cloud_run_v2_job.gdpr_wipeout_job.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.gdpr_wipeout_invoker.email}"
+}
+
+# Cloud Scheduler job to run every hour
+resource "google_cloud_scheduler_job" "gdpr_wipeout_schedule" {
+  name        = "crm-gdpr-wipeout-schedule"
+  description = "Trigger GDPR wipeout job every hour"
+  schedule    = "0 * * * *"
+  time_zone   = "UTC"
+  region      = "asia-east1"
+  project     = var.project_id
+
+  retry_config {
+    retry_count = 3
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${google_cloud_run_v2_job.gdpr_wipeout_job.location}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.project_id}/jobs/${google_cloud_run_v2_job.gdpr_wipeout_job.name}:run"
+
+    oauth_token {
+      service_account_email = google_service_account.gdpr_wipeout_invoker.email
+    }
+  }
+
+  depends_on = [
+    google_project_service.gdpr_wipeout_apis
   ]
 }
